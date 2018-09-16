0e19a009b3dd4a9586cb650f16929285217ae9db
Minwoo Kim <mwkim0919@gmail.com>
2018-09-16 23:28:13 +0000

    Merged in project-setup (pull request #1)
    project setup + bitbucket setup test

---
+++ src/main/java/com/ez/ezbackend/EzBackendApplication.java
@@ -0,0 +1,12 @@
+package com.ez.ezbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzBackendApplication.class, args);
+	}
+}
