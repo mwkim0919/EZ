ab692201a8f949268b98ccf249b719c7f12df869
mwkim0919 <mwkim0919@gmail.com>
2018-11-11 01:00:31 -0800

    fixing error response format

--- backend/src/main/java/com/ez/ezbackend/shared/security/JwtAuthenticationEntryPoint.java
+++ backend/src/main/java/com/ez/ezbackend/shared/security/JwtAuthenticationEntryPoint.java
@@ -1,24 +1,29 @@
 package com.ez.ezbackend.shared.security;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Slf4j
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+  @Inject
+  @Qualifier("handlerExceptionResolver")
+  private HandlerExceptionResolver handlerExceptionResolver;
+
   @Override
-  public void commence(HttpServletRequest httpServletRequest,
-                       HttpServletResponse httpServletResponse,
-                       AuthenticationException e) throws IOException {
+  public void commence(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AuthenticationException e) {
     log.error("Responding with unauthorized error. Message - {}", e.getMessage());
-    httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED,
-        "Sorry, You're not authorized to access this resource.");
+    handlerExceptionResolver.resolveException(request, response, null, e);
   }
 }
